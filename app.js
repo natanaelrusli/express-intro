d17dffa9a335572881941dd54fa3d18a36668cb6
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2021-01-08 10:50:06 +0700

    Simplify

--- app.js
+++ app.js
@@ -1,21 +1,84 @@
-// This is the example of creating back end API services without express.js
-// Creating using pure node and node http module
-const http = require('http');
-const port = 5000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res) => {
-    if (req.url === '/') {
-        res.write('<h1>HELLOO</h1>');
-        res.end();
+app.use(express.json());
 
-    } else if (req.url === '/name') {
-        res.write('<h1>My Name is Koding</h1>');
-        res.end();
+// Data array
+// This data will act like a database
+const posts = [
+    {id: 1, title: 'Post 1',description : 'Post Description 1'},
+    {id: 2, title: 'Post 2',description : 'Post Description 2'},
+    {id: 3, title: 'Post 3',description : 'Post Description 3'},
+];
 
-    }
+app.get('/', (req, res) => {
+    // You can give response in html format
+    res.send('<h1>Hello World!</h1><p>This is a project using Express JS</p>');
 });
 
-server.listen(port);
-console.log(`Listening on port ${port}`)
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+// This route will get one specific data and show it, if the data is not found it will show error 404, data is not found
+// The URI should be like : /posts/1
+app.get('/posts/:id', (req, res) => {
+    const post = posts.find((element) => element.id === parseInt(req.params.id));
+    if(!post) {
+        res.status(404).send('The data is not found');
+        return;   
+    }
+    
+    res.send(post);
+});
 
 
+app.post('/posts',(req,res) => {
+    const post = {
+        id : posts.length + 1,
+        title: req.body.title,
+        description: req.body.description,
+    };
+    
+    posts.push(post);
+    res.send(post);
+});
+
+app.put('/posts/:id', (req,res) => {
+    // Look up the data
+    // If not existing, return 404
+    const post = posts.find((element) => element.id === parseInt(req.params.id));
+    if(!post) {
+        res.status(404).send('The data is not found');
+        return;   
+    }
+    
+    // Update data
+    // Return the updated data to the client
+    post.title = req.body.title;
+    res.send(post);
+});
+
+app.delete('/posts/:id', (req,res) => {
+    // Look up for the data with given id
+    const post = posts.find((element) => element.id === parseInt(req.params.id));
+    if(!post) {
+        res.status(404).send('The data is not found');
+        return;   
+    }
+
+    // Delete data
+    const index = posts.indexOf(post);
+    posts.splice(index, 1);
+
+    // Return the data
+    res.send(post);
+});
+
+// PORT
+const port = 5000;
+
+// Creating a port listener using express.js
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
